b1388b7d0a95fd9a7850e727bd001ff5d27667e2
Terrence Kuiper <github@terrencekuiper.com>
2019-09-26 19:03:42 -0700

    refactored

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchCoordsByIP('207.194.49.162', (error, coord) => {
 //   if (error) {
@@ -18,12 +18,12 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 //   console.log('It worked! Returned IP:', ip);
 // });
 
-const myCoords = { latitude: '43.63190', longitude: '-79.37160' }
+// const myCoords = { latitude: '43.63190', longitude: '-79.37160' }
 
-fetchISSFlyOverTimes(myCoords, (error, passTime) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-  console.log('It worked! Details:', passTime);
-});
+// fetchISSFlyOverTimes(myCoords, (error, passTime) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Details:', passTime);
+// });
