4bc2be1dd4e7214297c86e5b075b129aaf95ac24
Terrence Kuiper <github@terrencekuiper.com>
2019-09-26 18:24:51 -0700

    fetchCoordsByIP added with tests

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('207.194.49.162', (error, coord) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:', coord);
+});
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
--- iss.js
+++ iss.js
@@ -6,9 +6,24 @@ const fetchMyIP = function(callback) {
 
     const data = JSON.parse(body);
     callback(null, data.ip);
-
   }
   );
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body).data;
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
